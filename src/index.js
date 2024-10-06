2f586710c34da26ed183a276d5574028f79e573d
unknown <dickensallinone@gmail.com>
2024-10-06 16:18:39 +0800

    add new things for refactoring

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
 
      <Provider store={store}>
       <App /> 
