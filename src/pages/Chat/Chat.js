1ecee658485f52bdbf18d143b68156097d953ddc
Syvhell Abad Umila <syvhellabadumila@gmail.com>
2024-10-08 21:22:06 +0800

    added props drilled userId

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,23 +1,29 @@
 import React, { useContext } from "react";
 import UserContext from "../../UserContext";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import SideMenu from "../../components/SideMenu/SideMenu";
 import DashboardContainer from "../../components/DashboardContainer/DashboardContainer";
 import dashboard from "./Chat.module.css";
-const Chat = ()=>{
-    const {user}= useContext(UserContext);
-    return(
-        <main>
-        {!user?.id && <Navigate to={"/login"}/>}
 
-        {user?.id &&  <section className={dashboard['dashboard']}>
-             <SideMenu/>
-             <DashboardContainer>
-                 {/* {user.userType === 'senior' && <ProfileCard/>}     */}
-             </DashboardContainer>            
-         </section>}
-     </main>        
-    )
-}
+const Chat = () => {
+  const { user } = useContext(UserContext);
+  const location = useLocation();
+  const { recipientId } = location.state;
+  return (
+    <main>
+      {!user?.id && <Navigate to={"/login"} />}
+
+      {user?.id && (
+        <section className={dashboard["dashboard"]}>
+          <SideMenu />
+          <DashboardContainer>
+            this is the drilled userID {recipientId}
+            {/* {user.userType === 'senior' && <ProfileCard/>}     */}
+          </DashboardContainer>
+        </section>
+      )}
+    </main>
+  );
+};
 
 export default Chat;
