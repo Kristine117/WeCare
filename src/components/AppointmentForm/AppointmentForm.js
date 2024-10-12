f4a1a91fc047a7e0169fe4d0022a49e7b72c7be6
unknown <dickensallinone@gmail.com>
2024-10-12 21:43:05 +0800

    fix appointment request

--- src/components/AppointmentForm/AppointmentForm.js
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -1,6 +1,7 @@
+import React from "react-router-dom";
 import "../../components/css/Appointment.css";
 import wcdesign from "./AppointmentForm.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 const AppointmentForm = ({ assistantId, fullName }) => {
@@ -8,7 +9,33 @@ const AppointmentForm = ({ assistantId, fullName }) => {
   const appointmentDate = today;
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [assistant,setAssistant] = useState(null);
 
+  useEffect(()=>{
+    async function getAssistantDetails(){
+        try{
+          const data = await fetch(`${process.env.REACT_APP_API_URL}/main/assistant-details/${assistantId}`,{
+            headers:{
+              "Authorization": `Bearer ${localStorage.getItem("token")}`,
+            }
+          })
+
+          if(!data.ok){
+            throw new Error("Something went wrong");
+          }
+
+          const newData = await data.json();
+
+          setAssistant(newData?.data)
+        }catch(e){
+          throw new Error(e.message)
+        }
+    }
+
+    getAssistantDetails();
+  },[])
+
+  console.log(assistant);
   function sendAppointment(e) {
     e.preventDefault();
 
@@ -51,7 +78,7 @@ const AppointmentForm = ({ assistantId, fullName }) => {
   return (
     <div className={wcdesign["form-container"]}>
       <h3 className="head"> Appointment Details</h3>
-      <p>Assistant Name: {fullName}</p>
+      <p>Assistant Name: {assistant?.fullName}</p>
       <form onSubmit={(e) => sendAppointment(e)}>
         <div className="form-group">
           <label>Service Start Date</label>
@@ -69,6 +96,15 @@ const AppointmentForm = ({ assistantId, fullName }) => {
             id="startDate"
             className="form-control"
             onChange={(e) => setEndDate(e.target.value)}
+          />
+        </div>
+
+
+        <div className="form-group">
+          <label htmlFor="service-duration">Service Duration</label>
+          <input type="number"
+          id="service-duration"
+          className="form-control"
           />
         </div>
 
