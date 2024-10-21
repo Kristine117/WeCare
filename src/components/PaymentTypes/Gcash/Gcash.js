d78789b49c88e0f27cff39b9e68d717acf40ef62
unknown <dickensallinone@gmail.com>
2024-10-21 11:32:06 +0800

    add new modification paymaya, gcash

--- src/components/PaymentTypes/Gcash/Gcash.js
+++ src/components/PaymentTypes/Gcash/Gcash.js
@@ -1,8 +1,19 @@
 import React from "react";
-
-const Gcash = ()=>{
+import Button from "../../Button/Button";
+import { BiArrowBack } from "react-icons/bi";
+import gh from "./Gcash.module.css";
+const Gcash = ({handleBackFunc})=>{
     return (
-        <div>Gcash</div>
+        <React.Fragment>
+           <header className={gh["header"]}> 
+                <BiArrowBack className={gh["back"]} onClick={handleBackFunc}/> 
+                <img src="./gcash.png" className={gh["img"]}/>      
+            </header>     
+            <div className={gh["container"]}>   
+                <input name="phone-number" type="number" className={gh["phone-no"]}/>
+                <Button type="button">Pay</Button>
+            </div>
+        </React.Fragment>
     )
 }
 export default Gcash;
--- src/components/PaymentTypes/Paymaya/Paymaya.js
+++ src/components/PaymentTypes/Paymaya/Paymaya.js
@@ -1,10 +1,20 @@
 import React from "react";
+import gh from "./Paymaya.module.css";
+import { BiArrowBack } from "react-icons/bi";
+import Button from "../../Button/Button";
 
-const Paymaya = ()=>{
+const Paymaya = ({handleBackFunc})=>{
     return (
-        <div>
-            Paymaya
-        </div>
+        <React.Fragment>
+           <header className={gh["header"]}> 
+                <BiArrowBack className={gh["back"]} onClick={handleBackFunc}/> 
+                <img src="./paymaya.png" className={gh["img"]}/>      
+            </header>     
+            <div className={gh["container"]}>   
+                <input name="phone-number" type="number" className={gh["phone-no"]}/>
+                <Button type="button">Pay</Button>
+            </div>
+        </React.Fragment>
     )
 }
 
