1659dd424bc64324fd411090ee40a3649475626c
unknown <dickensallinone@gmail.com>
2024-10-24 19:28:02 +0800

    fix display of data in the admin card details

--- src/components/UserListTable/UserListTable.js
+++ src/components/UserListTable/UserListTable.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import kwan from "./UserListTable.module.css";
-import { FaEllipsisV } from "react-icons/fa";
+import {FaEllipsisH } from "react-icons/fa";
+import Button from "../Button/Button";
+import { createPortal } from "react-dom";
 
 const UserListTable=({length, list})=>{
     const [allBoxes,setAllBoxes]= useState(false);
+    const [openModal,setOpenModal] = useState(false);
+    const [openFloat,setOpenFloat]= useState(
+        new Array(length).fill(false)
+      );
     const [checkedState, setCheckedState] = useState(
         new Array(length).fill(false)
       );
-
 
     const handleOnChange = (position) => {
 
@@ -24,8 +29,26 @@ const UserListTable=({length, list})=>{
         setCheckedState(updatedCheckedState);
     };
 
+    function openFloatFunc(e){
+        const floatMap = openFloat?.map((item, index)=> index === +e.target.dataset.index );
+    
+        setOpenFloat(floatMap)
+    }
+
+    const modal = openModal && createPortal(<>
+    <div className={kwan["backdrop-modal"]} onClick={()=>{setOpenModal(val=>!val)
+        setOpenFloat(new Array(length).fill(false))
+    }}></div>
+    <div className={kwan["container"]}>
+        
+    </div>
+    </>
+    ,document.querySelector("#modal"))
+
+
     return(
         <React.Fragment>
+            {modal}
             <header className={kwan["header"]}>
                 <div><input type="checkbox" name="main" checked={allBoxes} onChange={()=>handleOnChange(-1)}/></div>
                 <div>Name</div>
@@ -44,7 +67,11 @@ const UserListTable=({length, list})=>{
                    <div>{val.email}</div>
                    <div>{val.userType === 'assistant' ? "Assistant": "Senior"}</div>
                    <div>{val.approveFlg ? "Verified": "Pending"}</div>
-                   <FaEllipsisV className={kwan["ellipsis"]}/>
+                   <FaEllipsisH className={kwan["ellipsis"]} data-index={i} onClick={openFloatFunc}/>
+                   {openFloat[i] && <div className={kwan["floating-option"]}>
+                        <Button type="button" className={kwan["btn-edit"]}>Edit</Button>
+                        <Button type="button" className={kwan["btn-delete"]} onClick={()=>setOpenModal(val=>!val)}>Delete</Button>
+                   </div>}
                 </li>)}
             </ul>
         </React.Fragment>
