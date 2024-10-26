3fa0ceddafaf65e1bb461390d4f0062590c9faca
unknown <dickensallinone@gmail.com>
2024-10-26 19:09:58 +0800

    fix routing of pending assistant profile

--- src/components/RequestList/RequestList.js
+++ src/components/RequestList/RequestList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import design from "./RequestList.module.css";
 import Button from "../Button/Button";
+import { Link } from "react-router-dom";
 
 const RequestList = ({list,loading})=>{
     console.log(list)
@@ -8,8 +9,11 @@ const RequestList = ({list,loading})=>{
         <ul className={design["container"]}>
             {list?.map(val=>{
                 return <li key={val.userId} className={design["list-item"]}>
-                    <img src={val.profileImage} alt="Some Image"/>
-                    <div>{val.fullName}</div>
+                    <img src={val.profileImage} alt="Some Image" className={design["img"]}/>
+                    <div>
+                        <div><strong>{val.fullName}</strong> wants to register</div>
+                        <Link to={`requests/${val.userId}`}>Read Application</Link>    
+                    </div>
                     {!val.approveFlg && 
                     <div className={design["controller"]}>    
                         <Button type="button" className={design["approve-btn"]}>Approve</Button>
