2c9d702d2e6b745116629e9acd37735e0492802b
unknown <dickensallinone@gmail.com>
2024-10-26 18:29:14 +0800

    add new changes on the route, add new components and pages

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import Requests from "./pages/Requests/Requests";
 import Users from "./pages/Users/Users";
 import Notes from "./pages/Notes/Notes";
 import Profile from "./pages/Profile/Profile";
+import RequestAssistantDetails from "./pages/RequestAssistantDetails/RequestAssistantDetails";
 
 function App() {
   // for getting token this is for global this is helped whit the UserContext.js
@@ -91,8 +92,8 @@ function App() {
     <UserProvider value={{ user, setUser, unsetUser,appListStatus,setAppListStatus }}>
       <Router>
         <Routes>
-          <Route path="*" errorElement={<Error />} />
-          <Route path="/" element={<HomeContainer />} />
+        <Route path="/" element={<HomeContainer />} />
+      
           <Route path="/login" element={<Login />} />
           <Route path="/registration1" element={<Registration1 />} />
           <Route path="/registration3" element={<Registration3 />} />
@@ -109,12 +110,17 @@ function App() {
             element={<Appointment />}
           />
           <Route path="/appointment" element={<AppointmentList />} />
-
           <Route path="/find" element={<Find />} />
           <Route path="/support" element={<Support />} />
           <Route path="/ratings" element={<Ratings />} />
-          <Route path="/requests" element={<Requests />} />
+
+          <Route path="requests/" >
+            <Route index element={<Requests />}/>
+            <Route path=":userId" element={<RequestAssistantDetails/>}/>
+          </Route>
+
           <Route path="/users" element={<Users />} />
+          <Route path="*" errorElement={<Error />} />
         </Routes>
       </Router>
     </UserProvider>
--- src/pages/Requests/Requests.js
+++ src/pages/Requests/Requests.js
@@ -1,13 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect,useState } from "react";
 import UserContext from "../../UserContext";
 import { Navigate } from "react-router-dom";
 import ds from "./Requests.module.css";
 import SideMenu from "../../components/SideMenu/SideMenu";
 import DashboardContainer from "../../components/DashboardContainer/DashboardContainer";
 import LoggedInCommonNavBar from "../../components/LoggedInCommonNavBar/LoggedInCommonNavBar";
+import ListController from "../../components/ListController/ListController";
+import RequestList from "../../components/RequestList/RequestList";
+
+const APP_LIST_STATUS = [
+    { btnName: "requests", btnTitle: "Requests" },
+    { btnName: "history", btnTitle: "History" },
+  ];
+
 
 const Requests = ()=>{
     const {user} = useContext(UserContext);
+
+    const [list,setList]= useState(null);
+    const [status,setStatus]= useState("requests");
+    const [loading,setLoading] = useState(false);
+    useEffect(() => {
+    const fetchData = () => {
+        setLoading(true)
+        fetch(`${process.env.REACT_APP_API_URL}/admin/assistant-applicants`, {
+            headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+        
+            setList(data.data);
+            setLoading(false);
+            });
+        };
+    fetchData();
+
+
+    }, [status]);
+
+    function switchListRequestsFunc(e){
+        setStatus(e.target.name);
+    }
+
 
     return(
         <main>
@@ -16,6 +52,8 @@ const Requests = ()=>{
             <SideMenu/>
             <DashboardContainer>
                 <LoggedInCommonNavBar title="Requests"/>
+                <ListController btnList={APP_LIST_STATUS} status={status} switchListRequests={switchListRequestsFunc}/>
+                <RequestList list={list?.assistantListPending}/>
             </DashboardContainer>            
         </section>}
     </main> 
