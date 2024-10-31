8fb94cf77379e23e6851341630814fa2044a62bd
unknown <dickensallinone@gmail.com>
2024-10-31 15:29:40 +0800

    add new changes for fetching and fix issue parsing body in the front end and make it readable in the back end

--- src/pages/RequestAssistantDetails/RequestAssistantDetails.js
+++ src/pages/RequestAssistantDetails/RequestAssistantDetails.js
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useLoaderData, useParams } from "react-router-dom";
 import SideMenu from "../../components/SideMenu/SideMenu";
 import DashboardContainer from "../../components/DashboardContainer/DashboardContainer";
 import design from "./RequestAssistantDetails.module.css";
 import UserContext from "../../UserContext";
+
 const RequestAssistantDetails = ()=>{
     const {user} = useContext(UserContext);
     const {userId} = useParams();
-    console.log(userId)
+
+
     return(
         <main>
              {(user?.userType !== "admin" && user?.userType !== null ) && <Navigate to={"/login"}/>}
@@ -16,6 +18,10 @@ const RequestAssistantDetails = ()=>{
                 <DashboardContainer>
                     <div className={design["assistant-card"]}>
                         <h1>Assistant Profile</h1>
+
+                        <img src={user.profileImage} alt="Profile Image"/>
+
+                        <div>{user.firstname} {user.lastname}</div>
                     </div>      
                 </DashboardContainer>           
             </section> 
@@ -24,4 +30,19 @@ const RequestAssistantDetails = ()=>{
 }
 
 export default RequestAssistantDetails;
+
+export async function retrieveUserDetailsFunc({request,params}){
+    const {applicantId} = await params;
+    console.log(applicantId);
+    let data;
+    try{
+        let initial = await fetch(`${process.env.REACT_APP_API_URL}/admin/assistant-details`)
+    }catch(e){
+        throw new Error(e.message);
+    }
+
+    return {
+        kwankwan: "kwankwan"
+    }
+}
 
